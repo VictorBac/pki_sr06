d411d43b1afd068920fb62dd218858a35de6d913
Florent Thévenet <florent@fthevenet.fr>
2015-11-25 16:19:19 +0100

    Lister les slots d'un module PKCS11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
 package fr.utc.sr06.CryptokiExplorer;
 
+import iaik.pkcs.pkcs11.Module;
+import iaik.pkcs.pkcs11.Slot;
+import iaik.pkcs.pkcs11.TokenException;
+
+import java.io.IOException;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("coucou");
+        System.out.println("il a pas dit bonjour.");
+
+        try {
+            Module m = Module.getInstance("/usr/local/lib/softhsm/libsofthsm2.so");
+            m.initialize(null);
+
+            Slot[] slots = m.getSlotList(Module.SlotRequirement.ALL_SLOTS);
+            for (Slot slot : slots) {
+                System.out.format("Slot: %d\n%s", slot.getSlotID(), slot.getModule());
+            }
+
+            m.finalize(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TokenException e) {
+            e.printStackTrace();
+        }
+
     }
 }
