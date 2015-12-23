d7fd3f43b09f75a27f40a483f9d53bd02ab48022
RapaelKovacic <raphael.kovacic@gmail.com>
2015-12-23 11:57:00 +0100

    Pouet Pouet

---
+++ src/main/java/fr/utc/sr06/CryptokiExplorer/Window.java
@@ -0,0 +1,34 @@
+package fr.utc.sr06.CryptokiExplorer;/**
+ * Created by raphael on 23/12/15.
+ */
+
+import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+
+import javafx.geometry.Pos;
+
+import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.layout.GridPane;
+
+import javafx.stage.Stage;
+
+
+public class Window extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
