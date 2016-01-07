6529a4f0ada5b94b9b41ffd74df3e10a7f1b9335
Florent Thévenet <florent@fthevenet.fr>
2016-01-07 13:08:26 +0100

    Interface sélection pour les mechanismes

--- src/main/java/fr/utc/sr06/CryptokiExplorer/MechanismsFunction.java
+++ src/main/java/fr/utc/sr06/CryptokiExplorer/MechanismsFunction.java
@@ -1,9 +1,17 @@
 package fr.utc.sr06.CryptokiExplorer;
 
 import iaik.pkcs.pkcs11.*;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.util.Pair;
+import javafx.util.StringConverter;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -12,9 +20,12 @@ import java.util.ResourceBundle;
  * Created by florent on 31/12/15.
  */
 public class MechanismsFunction extends BaseUIFunction {
-    private TextArea ui = null;
+    private VBox ui = null;
     private ResourceBundle translations;
-    private SimpleStringProperty mechanims=new SimpleStringProperty();
+    private SimpleStringProperty mechanismInfo = new SimpleStringProperty();
+
+    private ChoiceBox<Pair<Mechanism, MechanismInfo>> select;
+    private ObservableList<Pair<Mechanism, MechanismInfo>> mechanisms;
 
     public MechanismsFunction(String name, ModuleManager manager) { // TODO: il nous faut un moyen d'afficher des erreurs
         super(name, manager);
@@ -23,31 +34,50 @@ public class MechanismsFunction extends BaseUIFunction {
 
     @Override
     public void load(Slot slot) {
-        ui = new TextArea();
-        ui.textProperty().bind(mechanims);
-        // TODO lancer sur un thread
+        ui = new VBox();
+
+        select = new ChoiceBox<>();
+        mechanisms = FXCollections.observableArrayList();
+        select.setItems(mechanisms);
+        select.setConverter(new StringConverter<Pair<Mechanism, MechanismInfo>>() {
+            @Override
+            public String toString(Pair<Mechanism, MechanismInfo> object) {
+                return object.getKey().getName();
+            }
+
+            @Override
+            public Pair<Mechanism, MechanismInfo> fromString(String string) {
+                return null;
+            }
+        });
+        select.getSelectionModel().selectedItemProperty().addListener((ov, old, new_val) -> {
+            mechanismInfo.set(new_val.getValue().toString());
+        });
+
+        TextArea infoArea = new TextArea();
+        infoArea.textProperty().bind(mechanismInfo);
+
         try {
             Token token = slot.getToken();
             if (token != null) {
-                StringBuilder data = new StringBuilder();
-
                 for (Mechanism mec: token.getMechanismList()) {
-                    data.append(mec.getName());
-                    data.append('\n');
-                    data.append(token.getMechanismInfo(mec).toString());
-                    data.append("\n\n");
+                    mechanisms.add(new Pair<>(mec, token.getMechanismInfo(mec)));
                 }
-                mechanims.set(data.toString());
-                //ui.setText(data.toString());
+
+                if (!mechanisms.isEmpty()) {
+                    Platform.runLater(() -> select.getSelectionModel().selectFirst());
+                    mechanismInfo.set(mechanisms.get(0).getValue().toString());
+                }
             } else {
-                mechanims.set(translations.getString("noToken"));
-                //ui.setText(translations.getString("noToken"));
+                mechanismInfo.set(translations.getString("noToken"));
             }
         } catch (TokenException e) {
             e.printStackTrace();
 
         }
 
+        ui.getChildren().setAll(select, infoArea);
+        ui.setVgrow(infoArea, Priority.ALWAYS);
     }
 
     @Override
